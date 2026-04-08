502ebf68225cbcde2a8ef07fdd4382bcabbcc529
okutagawa <diziek182@gmail.com>
2026-04-08 08:08:49 +0400

    разработка веб приложения ч.3

---
+++ PublishingReviewApp/Controllers/HomeController.cs
@@ -0,0 +1,6 @@
+﻿namespace PublishingReviewApp.Controllers
+{
+    public class HomeController
+    {
+    }
+}
---
+++ PublishingReviewApp/Models/PublicationModel.cs
@@ -0,0 +1,6 @@
+﻿namespace PublishingReviewApp.Models
+{
+    public class PublicationModel
+    {
+    }
+}
