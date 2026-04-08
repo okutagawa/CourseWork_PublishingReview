d4a1eb17e425412a089031882710ecc01c22a8aa
okutagawa <diziek182@gmail.com>
2026-04-08 09:12:24 +0400

    разработка веб приложения ч.7

--- PublishingReviewBusinessLogic/obj/Debug/net8.0/PublishingReviewBusinessLogic.AssemblyInfo.cs
+++ PublishingReviewBusinessLogic/obj/Debug/net8.0/PublishingReviewBusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublishingReviewBusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2727ab8d86a33960e702ad8d579e7d1d74b4f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c08cec975725da5ccdedb3313b267a2aec94760")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublishingReviewBusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PublishingReviewBusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
